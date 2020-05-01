4169b1989056ac2c9cf975ed96ae2c8b1cf416d2
DESKTOP-JD9LFA8\tanut <tanutapi@gmail.com>
2020-05-01 12:16:35 +0700

    Update test

--- test/dart_meteor_test.dart
+++ test/dart_meteor_test.dart
@@ -2,16 +2,8 @@ import 'package:dart_meteor/dart_meteor.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('MeteorClient', () {
-    MeteorClient meteor;
-
-    setUp(() {
-      meteor = MeteorClient.connect(url: 'wss://echo.websocket.org');
-    });
-
-    tearDown(() {
-      meteor.disconnect();
-    });
+  group('Environment', () {
+    MeteorClient meteor = MeteorClient.connect(url: 'ws://127.0.0.1:3000');
 
     test('meteor.isClient', () {
       expect(meteor.isClient(), isTrue);
@@ -23,6 +15,26 @@ void main() {
 
     test('meteor.isCordova', () {
       expect(meteor.isCordova(), isFalse);
+    });
+  });
+
+  group('Login', () {
+    MeteorClient meteor = MeteorClient.connect(url: 'ws://127.0.0.1:3000');
+
+    setUp(() async {
+      meteor.reconnect();
+      await Future.delayed(Duration(seconds: 2));
+    });
+
+    tearDown(() {
+      meteor.disconnect();
+    });
+
+    test('meteor.loginWithPassword', () async {
+      MeteorClientLoginResult result =
+      await meteor.loginWithPassword('user1', 'password1');
+      print('MeteorClientLoginResult: ' + result.toString());
+      expect(meteor.userId(), isNotNull);
     });
   });
 }
