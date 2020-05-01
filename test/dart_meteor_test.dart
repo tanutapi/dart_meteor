9edfe2e4fb49fde80eec9ea657746275fad057ae
tanutapi <tanutapi@gmail.com>
2020-05-01 11:55:36 +0700

    Testing (#3)
    * Test with ls and pwd
    
    * Try to setup docker-compose
    
    * Update dart.yml
    
    * Update test
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update test
    
    * test container
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update dart.yml
    
    * Update test

--- test/dart_meteor_test.dart
+++ test/dart_meteor_test.dart
@@ -2,39 +2,39 @@ import 'package:dart_meteor/dart_meteor.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('MeteorClient', () {
-    MeteorClient meteor;
+  group('Environment', () {
+    MeteorClient meteor = MeteorClient.connect(url: 'ws://127.0.0.1:3000');
 
-    setUp(() {
-      meteor = MeteorClient.connect(url: 'ws://localhost:3000');
+    test('meteor.isClient', () {
+      expect(meteor.isClient(), isTrue);
+    });
+
+    test('meteor.isServer', () {
+      expect(meteor.isServer(), isFalse);
+    });
+
+    test('meteor.isCordova', () {
+      expect(meteor.isCordova(), isFalse);
+    });
+  });
+
+  group('Login', () {
+    MeteorClient meteor = MeteorClient.connect(url: 'ws://127.0.0.1:3000');
+
+    setUp(() async {
+      meteor.reconnect();
+      await Future.delayed(Duration(seconds: 2));
     });
 
     tearDown(() {
       meteor.disconnect();
     });
-//
-//    test('meteor.isClient', () {
-//      expect(meteor.isClient(), isTrue);
-//    });
-//
-//    test('meteor.isServer', () {
-//      expect(meteor.isServer(), isFalse);
-//    });
-//
-//    test('meteor.isCordova', () {
-//      expect(meteor.isCordova(), isFalse);
-//    });
 
     test('meteor.loginWithPassword', () async {
-      await Future.delayed(Duration(seconds: 5));
-      try {
-        MeteorClientLoginResult result =
-            await meteor.loginWithPassword('user1', 'password1');
-        print('MeteorClientLoginResult: ' + result.toString());
-        expect(meteor.userId(), isNotNull);
-      } catch (e) {
-        print(e);
-      }
+      MeteorClientLoginResult result =
+          await meteor.loginWithPassword('user1', 'password1');
+      print('MeteorClientLoginResult: ' + result.toString());
+      expect(meteor.userId(), isNotNull);
     });
   });
 }
