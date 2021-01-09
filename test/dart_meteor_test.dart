774f2aaf98331c428dcc952cd91c4454f802f4c8
Tanut Apiwong <tanutapi@gmail.com>
2021-01-09 21:43:08 +0700

    Subscription test group

--- test/dart_meteor_test.dart
+++ test/dart_meteor_test.dart
@@ -69,6 +69,42 @@ void main() {
       expect(meteor.userId(), isNotNull);
     });
 
+    test('meteor.loggingIn', () async {
+      var state = 0;
+      var currentLoggingIn = true;
+      meteor.loggingIn().listen((event) {
+        print('Before: loggingIn state: $state, loggingIn: $event');
+        currentLoggingIn = event;
+        if (state == 0 && event == false) {
+          state++;
+        } else if (state == 1 && event == true) {
+          state++;
+        } else if (state == 2 && event == false) {
+          state++;
+        }
+        print('After: loggingIn state: $state, loggingIn: $event');
+      });
+      var result = await meteor.loginWithPassword('user1', 'password1');
+      print('MeteorClientLoginResult: ' + result.toString());
+      expect(meteor.userId(), isNotNull);
+      await Future.delayed(Duration(seconds: 5));
+      expect(state, 3);
+      expect(currentLoggingIn, false);
+    });
+  });
+
+  group('subscription', () {
+    var meteor = MeteorClient.connect(url: 'ws://127.0.0.1:3000');
+
+    setUp(() async {
+      meteor.reconnect();
+      await Future.delayed(Duration(seconds: 2));
+    });
+
+    tearDown(() {
+      meteor.disconnect();
+    });
+
     test('meteor.subscribe with onReady', () async {
       var completer = Completer();
       expect(completer.future, completion(true));
